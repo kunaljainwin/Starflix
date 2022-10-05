fc67f7793579290fdcfed08162bda7fa4935e864
Yash Kumar <87136660+Yashk786@users.noreply.github.com>
2022-10-05 18:19:41 +0530

    Change Dropdown UI and Logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,51 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import Dropdown from './dropdown';
-import "./Nav.css"
-function Nav(){
-    const[show,handleShow]=useState(false);
-    useEffect(() => {
-    window.addEventListener("scroll",()=>{
-        if(window.scrollY>100){
-            handleShow(true);
-        }
-        else{
-            handleShow(false);
+import React, { useEffect, useState } from "react";
+import Dropdown from "./dropdown";
+import "./Nav.css";
 
-        }
-
-    })
-    
-      return () => {
-  window.removeEventListener("scroll",()=>{  handleShow(false)})
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
       }
-    }, [])
-    
+    });
 
-    // useEffect(()=>{
+    return () => {
+      window.removeEventListener("scroll", () => {
+        handleShow(false);
+      });
+    };
+  }, []);
 
-    //     window.addEventListener("scroll",()=>{
-      
+  // useEffect(()=>{
 
-    //         if(window.scrollY>100){
-    //             handleShow(true);
-    //         }
-    //         else{
-    //             handleShow(false);
-    //         }
-    //     });
-    //     return()=>{
-    //         window.removeEventListener("scroll");
-    //     };
+  //     window.addEventListener("scroll",()=>{
 
-    // },[])
+  //         if(window.scrollY>100){
+  //             handleShow(true);
+  //         }
+  //         else{
+  //             handleShow(false);
+  //         }
+  //     });
+  //     return()=>{
+  //         window.removeEventListener("scroll");
+  //     };
 
+  // },[])
 
-    return(<div className={show ? 'nav':'nav-hide'}>
-         <img className='App-logo' src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo-500x281.png" alt='starflix'></img>
+  return (
+    <div className={show ? "nav" : "nav-hide"}>
+      <img className="App-logo" src="/logo.png" alt="starflix"></img>
       <Dropdown></Dropdown>
-         <img className='avatar' alt='starflix' src='https://www.logoai.com/oss/avatars/2022/06/06/2b01fcad6dd8c63df1cc56508379bb5c.jpeg'></img>
-
-    </div>);}
-
+      <img
+        className="avatar"
+        alt="starflix"
+        src="https://www.logoai.com/oss/avatars/2022/06/06/2b01fcad6dd8c63df1cc56508379bb5c.jpeg"
+      ></img>
+    </div>
+  );
+}
 
 export default Nav;
